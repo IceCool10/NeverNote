cedc3998ab5053fb03a7661d965a21b978d9a959
IceCool10 <cosmin.macovei23@gmail.com>
2017-05-09 22:47:24 +0300

    Tag added

---
+++ src/dbmodel/Tag.java
@@ -0,0 +1,16 @@
+package dbmodel;
+
+/**
+ * Created by root on 5/9/17.
+ */
+public class Tag {
+    private String Nume;
+
+    public Tag(String Nume) {
+        this.Nume   =   Nume;
+    }
+
+    public String getNume() {
+        return this.Nume;
+    }
+}

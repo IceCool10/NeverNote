945496fdd8b451845115cff42247e2c71bec44fe
IceCool10 <cosmin.macovei23@gmail.com>
2017-05-23 01:15:37 +0300

    Event handler on Create Notebook button.

--- src/NotesWindow/NewNotebookController.java
+++ src/NotesWindow/NewNotebookController.java
@@ -4,8 +4,12 @@ package NotesWindow;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-
+import javafx.event.*;
 import java.net.URL;
+import javafx.scene.*;
+import login.DBConnect;
+import login.User;
+
 import java.util.ResourceBundle;
 
 public class NewNotebookController implements Initializable {
@@ -15,13 +19,23 @@ public class NewNotebookController implements Initializable {
     public TextField notebookName;
     public Button createNotebook;
     public Label warningMessage;
+    private User user = DBConnect.getUser();
 
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resource) {
         this.fxmlFileLocation = fxmlFileLocation;
         this.resource = resource;
 
+        createNotebook.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                DBConnect.insertNotebook(notebookName.getText(),user.getUsername());
+            }
+        });
+
 
 
     }
+
+
 }
